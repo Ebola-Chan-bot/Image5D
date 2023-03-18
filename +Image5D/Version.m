ecc4c463c1fba921f82f2e5863923c5075374c4b
埃博拉酱 <vhtmfu@outlook.com>
2023-03-18 12:23:44 +0800

    增加自动更新检查

--- +Image5D/Version.m
+++ +Image5D/Version.m
@@ -2,3 +2,13 @@ function V=Version
 V.Me='1.5.1';
 V.MatlabExtension=MATLAB.Version;
 V.MATLAB='R2022b';
+persistent NewVersion
+try
+	if isempty(NewVersion)
+		NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/Silver-Fang/Image5D/releases','Image5D',V.Me);
+	end
+catch ME
+	if ME.identifier~="MATLAB:undefinedVarOrClass"
+		ME.rethrow;
+	end
+end
